650c3d7ca01bf785c6f644d0d8725905cc2ee286
Divindra Ramai <daveramai@hotmail.com>
2021-06-16 18:21:04 -0400

    Updated resolvers.js to include query user of review

--- resolvers.js
+++ resolvers.js
@@ -8,9 +8,21 @@ const resolvers = {
 
     Business: {
         name: (obj, args, context, info) => {
-        return obj.name
+            return obj.name
+        },
+        reviews: (obj, args, context, info) => {
+        return obj.reviewIds.map(v => { return context.db.reviews.find(review => { return review.reviewId === v;});});
+        }
+    },
+
+    Review: {
+        user: (obj, args, context, info) => {
+            return context.db.users.find( user => {return user.userId === obj.userId })
         }
     }
+
+        
+        
 }
 
 module.exports = resolvers;
